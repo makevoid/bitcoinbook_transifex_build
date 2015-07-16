a865835c0d2f25beca817a81c8b3307e8642f5ff
Francesco (makevoid) <makevoid@gmail.com>
2015-07-16 03:37:50 +0100

    ui is ok now

--- lib/languages.rb
+++ lib/languages.rb
@@ -1,3 +1,8 @@
 LANGUAGES = %i(
   en hu ja es fr pt_BR it zh_TW tr ca ro sv cmn vi nl de ru da pl ar bg cs fi fr_CA el ko ta th
 )
+
+LANGUAGES_FULL = %i[
+  english(source-text) hungarian japanese spanish french portugese(BR) italian taiwanense tr canadian rumenian sv chinese(mandarin) vietnam dutch german russian da polish
+  argentinian belgian cs finnish french(canada) el korean ta th
+]
--- ui/ui.rb
+++ ui/ui.rb
@@ -17,6 +17,10 @@ module AsciiFex
         LANGUAGES
       end
 
+      def lang_full(lang)
+        LANGUAGES_FULL[ LANGUAGES.index(lang) ]
+      end
+
       def sections
         %w(
           appendix-bips appendix-bx appendix-pycoin appendix-script-ops chapter-10
@@ -24,57 +28,17 @@ module AsciiFex
           chapter-8 chapter-9 glossary index praise preface.html
         )
       end
-
-      # alias :c :haml_concat
-      # alias :t :haml_tag
-
-
-      def sections_html
-        languages.map do |lang|
-          "
-          <h1>Language: #{lang}</h1>
-
-          <ul>
-            " +
-            sections.map do |section|
-              url    = "/#{lang}/#{section}"
-              gh_url = "https://github.com/makevoid/bitcoinbook_transifex_build/blob/with_translation/public/#{lang}/#{section}.asciidoc"
-              "
-              <li>
-                <section>
-                  <h1>
-                    <a href='#{url}.html'>#{section}</a>
-                    -
-                    <a href='#{gh_url}'>GH</a>
-                    -
-                    <a href='#{url}.asciidoc'>(source)</a>
-                  </h1>
-                </section>
-              </li>
-              "
-            end.join("\n") + "
-          </ul>
-
-          "
-
-          # haml_concat lang
-
-          # t(:h1){ c "Language: #{it}" }
-          # t :ul do
-          #   sections.map do |section|
-          #     t :li do
-          #       t(:a){ c "Language: #{it}" }
-          #     end
-          #   end
-          # end
-
-          # lang
-        end.join("\n")
-      end
     end
 
     get "/" do
       haml :index
+    end
+
+    get "/asciidoc/:lang/:name" do |lang, name|
+      content_type "text/plain"
+      path = "#{PATH}/public/translations/#{lang}/#{name}.asciidoc"
+      # send_file path, type: "text/plain", filename: "#{lang}-#{name}.txt"
+      File.read path
     end
   end
 end
