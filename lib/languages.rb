a1b2a4c6ac89c6aad889b5f00bcbd7694c43fd42
Francesco (makevoid) <makevoid@gmail.com>
2015-07-27 04:29:14 +0100

    finalize main page

--- lib/languages.rb
+++ lib/languages.rb
@@ -1,9 +1,9 @@
 LANGUAGES = %i(
-  en hu ja es fr pt_BR it zh_TW tr ca ro sv cmn vi nl de ru da pl ar bg cs fi fr_CA el ko ta th
+  en hu ja es fr it pt_BR zh_TW tr ca ro sv cmn vi nl de ru da pl ar bg cs fi fr_CA el ko ta th
 )
 
 LANGUAGES_FULL = %i[
-  english(source-text) hungarian japanese spanish french portugese(brazil) italian chinese(taiwan) turkish catalan
+  english(source-text) hungarian japanese spanish french italian portugese(brazil) chinese(taiwan) turkish catalan
   romanian swedish chinese(mandarin) vietnamese dutch german russian danish polish arabic bulgarian czech finnish
   french(canada) greek korean tamil thai
 ]
